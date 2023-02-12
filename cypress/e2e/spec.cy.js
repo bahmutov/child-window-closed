c58e3e92ac4247bed38fb34d7c7de74318fa695f
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-02-12 13:27:40 -0500

    working test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,18 @@
 /// <reference types="cypress" />
 
 it('opens the child window', () => {
-  cy.visit('index.html')
+  const mockWindow = {}
+  cy.visit('index.html').then((win) => {
+    cy.stub(win, 'open').returns(mockWindow).as('open')
+  })
+  cy.contains('a', 'link').click()
+  cy.get('@open').should('have.been.calledWith', 'child.html')
+  cy.get('.overlay')
+    .should('be.visible')
+    .wait(1000)
+    .then(() => {
+      // set parent window is watching the "window.closed" property
+      mockWindow.closed = true
+    })
+  cy.get('.overlay').should('not.be.visible')
 })
